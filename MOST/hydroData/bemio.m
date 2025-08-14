ae1fb775e74dc6552fb92879d4e97f4eebc5277a
Adam Keester <72414466+akeeste@users.noreply.github.com>
2025-08-14 13:49:32 -0500

    Update MOST application to v2 (#97)
    * replace MOST application with MOREnergyLab/MOST/VolturnUS plus QTFs
    
    * refactor MOST regression test
    
    * force add mass properties mat file
    
    * force add blade .dat files
    
    * fix path in TestMost.m
    
    * make test cd commands more robust
    
    * fix BEMIO path
    
    * force add .dat files for MOST hydroData
    
    * fix bemio path again
    
    * turn mostIO scripts into functions so they dont clear the test case
    
    * fix path again
    
    * fix file name capitalization
    
    * save case output, remove constant tests, update test results
    
    * comment out MOST regression test

--- MOST/hydroData/bemio.m
+++ MOST/hydroData/bemio.m
@@ -1,6 +1,6 @@
 
 hydro = struct();
-hydro = readNEMOH(hydro,fullfile('VolturnUS15MW_nemoh'));
+hydro = readNEMOH(hydro,'VolturnUS');
 hydro = radiationIRF(hydro,90,201,201,[],[]);
 hydro = radiationIRFSS(hydro,[],[]);
 hydro = excitationIRF(hydro,90,[],[],[],[]);
